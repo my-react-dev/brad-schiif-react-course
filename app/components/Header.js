ec01d20d0dc0091ae7bcfe26d6171d9c918423bc
Jahid Hasan <jahid0676@gmail.com>
2023-03-22 20:52:08 +0600

    Added Conditional Homepage Content

--- app/components/Header.js
+++ app/components/Header.js
@@ -3,9 +3,7 @@ import { Link } from "react-router-dom"
 import HeaderLoggedIn from "./HeaderLoggedIn"
 import HeaderLoggedOut from "./HeaderLoggedOut"
 
-function Header() {
-  const [loggedIn, setLoggedin] = useState(Boolean(localStorage.getItem("complexappToken")))
-
+function Header(props) {
   return (
     <header className="header-bar bg-primary mb-3">
       <div className="container d-flex flex-column flex-md-row align-items-center p-3">
@@ -14,7 +12,7 @@ function Header() {
             ComplexApp
           </Link>
         </h4>
-        {loggedIn ? <HeaderLoggedIn setLoggedin={setLoggedin} /> : <HeaderLoggedOut setLoggedin={setLoggedin} />}
+        {props.loggedIn ? <HeaderLoggedIn setLoggedin={props.setLoggedin} /> : <HeaderLoggedOut setLoggedin={props.setLoggedin} />}
       </div>
     </header>
   )
--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -9,21 +9,21 @@ function HeaderLoggedIn({ setLoggedin } = props) {
   }
 
   return (
-    <div class="flex-row my-3 my-md-0">
-      <a href="#" class="text-white mr-2 header-search-icon">
-        <i class="fas fa-search"></i>
+    <div className="flex-row my-3 my-md-0">
+      <a href="#" className="text-white mr-2 header-search-icon">
+        <i className="fas fa-search"></i>
       </a>
-      <span class="mr-2 header-chat-icon text-white">
-        <i class="fas fa-comment"></i>
-        <span class="chat-count-badge text-white"> </span>
+      <span className="mr-2 header-chat-icon text-white">
+        <i className="fas fa-comment"></i>
+        <span className="chat-count-badge text-white"> </span>
       </span>
-      <a href="#" class="mr-2">
-        <img class="small-header-avatar" src={localStorage.getItem("complexappAvatar")} />
+      <a href="#" className="mr-2">
+        <img className="small-header-avatar" src={localStorage.getItem("complexappAvatar")} />
       </a>
-      <a class="btn btn-sm btn-success mr-2" href="/create-post">
+      <a className="btn btn-sm btn-success mr-2" href="/create-post">
         Create Post
       </a>
-      <button onClick={handleLogout} class="btn btn-sm btn-secondary">
+      <button onClick={handleLogout} className="btn btn-sm btn-secondary">
         Sign Out
       </button>
     </div>
