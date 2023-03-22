3fde69d608ed1be2167f4cdafac826a6cb5feaa0
Jahid Hasan <jahid0676@gmail.com>
2023-03-22 19:39:12 +0600

    Add login componet and allow to login

---
+++ app/components/HeaderLoggedOut.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react"
+import Axios from "axios"
+
+function HeaderLoggedOut() {
+  const [username, setUsername] = useState()
+  const [password, setPassword] = useState()
+
+  async function handleLogin(e) {
+    e.preventDefault()
+    try {
+      const respons = await Axios.post("http://localhost:8080/login", { username, password })
+      if (respons.data) {
+        console.log(respons.data)
+      } else {
+        console.log("Incorrect Username or Password")
+      }
+    } catch (err) {
+      console.log("There was a problem")
+    }
+  }
+
+  return (
+    <form onSubmit={handleLogin} className="mb-0 pt-2 pt-md-0">
+      <div className="row align-items-center">
+        <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
+          <input onChange={e => setUsername(e.target.value)} name="username" className="form-control form-control-sm input-dark" type="text" placeholder="Username" autoComplete="off" />
+        </div>
+        <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
+          <input onChange={e => setPassword(e.target.value)} name="password" className="form-control form-control-sm input-dark" type="password" placeholder="Password" />
+        </div>
+        <div className="col-md-auto">
+          <button className="btn btn-success btn-sm">Sign In</button>
+        </div>
+      </div>
+    </form>
+  )
+}
+
+export default HeaderLoggedOut
